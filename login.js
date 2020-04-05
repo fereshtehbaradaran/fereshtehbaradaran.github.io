d820c56feb2c414e93e67f125ded036d634492af
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2020-04-05 23:22:39 +0430

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".toSignup").click(function(){
-        $("#loginbox").hide();
-        $("#signupBox").css("display","block");
-        $("title").text("Sign Up");
-    })
-})
